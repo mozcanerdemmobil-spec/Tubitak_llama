2be3634bcb60c123ae891e61135939a4033707d3
mozcanerdemmobil-spec <mozcanerdem.mobil@gmail.com>
2026-03-31 22:57:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,18 +17,21 @@ st.title("🎓 MEB Ortaöğretim Yönetmelik Asistanı")
 
 pdf_url = "https://raw.githubusercontent.com/mozcanerdemmobil-spec/Tubitak_llama/main/05150236_sinifprogrami.pdf"
 
-def show_pdf(url):
-    response = requests.get(url)
-    base64_pdf = base64.b64encode(response.content).decode("utf-8")
-
-    pdf_display = f"""
-    <embed src="data:application/pdf;base64,{base64_pdf}" 
-    width="100%" height="600px" type="application/pdf">
-    """
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
 if st.button("📄 PDF Aç"):
-    show_pdf(pdf_url)
+    response = requests.get(pdf_url)
+
+    with open("temp.pdf", "wb") as f:
+        f.write(response.content)
+
+    with open("temp.pdf", "rb") as f:
+        st.download_button(
+            label="📥 PDF indir",
+            data=f,
+            file_name="sinif_programi.pdf",
+            mime="application/pdf"
+        )
+
+    st.write("PDF'i görüntülemek için yukarıdan indir 👆")
 
 
 # --- 2. VERİ TABANI VE MODEL HAZIRLIĞI ---
