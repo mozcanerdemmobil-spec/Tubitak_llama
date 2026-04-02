8e1037659604f37ba9de4e70041dc3d0fd4aad3d
mozcanerdemmobil-spec <mozcanerdem.mobil@gmail.com>
2026-04-02 12:25:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,39 @@ Kritik Kurallar:
 1. SADECE 'Bağlam' içindeki bilgileri kullan.
 2. Cevap yoksa 'Yönetmelikte net bilgi bulamadım' de.
 3. Cevaplar maddeler halinde ve resmi olsun.
+4. Genel Bilgiler:
+    -Ders Saati Süresi Okulda 40 dakika , işletmelerde 60 dakikadır
+    -Ders Yılı Tanımı Derslerin başladığı tarihten kesildiği tarihe kadar olan süredir.
+    -Eğitim ve Öğretim Yılı Ders yılının başladığı tarihten ertesi ders yılının başladığı tarihe kadar olan süredir.
+5. Kayıt ve Geçiş:
+    -Yaş Sınırı Kayıt günü itibarıyla 18 yaşını bitirmemiş olmak gerekir.
+    -Evlilik Durumu Evli olanların kaydı yapılmaz; öğrenciyken evlenenlerin kaydı Açık Lise'ye aktarılır.
+    -Hazırlıkta Başarısızlık Üst üste iki yıl başarısız olan öğrenci, hazırlık sınıfı olmayan bir okulun 9. sınıfına nakledilir.
+6. Devamsızlık:
+    -Özürsüz Sınırı Özürsüz 10 günü geçen öğrenci başarısız sayılır.
+    -Toplam Sınır Özürlü ve özürsüz toplam devamsızlık 30 günü aşamaz.
+    -60 Günlük İstisna Organ nakli, ağır hastalık veya tutukluluk gibi hallerde toplam sınır 60 gündür.
+    -Geç Gelme Sadece birinci ders saati için geçerlidir; sonrası devamsızlıktan sayılır.
+7. Sınıf Geçme:
+    -Yazılı Sınav Sayısı Haftalık ders saatinden bağımsız olarak her dersten en az 2 yazılı yapılır.
+    -Başarı Puanı Bir dersten başarılı sayılmak için yılsonu puanının en az 50 olması gerekir.
+    -Sorumlu Geçme Bir sınıfta en fazla 3 dersten başarısız olanlar sorumlu geçer.
+    -Sınıf Tekrarı Toplam başarısız ders sayısı 6'yı geçerse öğrenci sınıf tekrar eder.
+    -Beceri Sınavı Puanın %80'i sınavdan, %20'si iş dosyasından gelir
+8. Nakil İşlemleri:
+    -Başvuru Zamanı Aralık ve Mayıs ayları hariç her ayın ilk iş günü başvurulabilir.
+    -Ders Seçimi (9.Sınıf) Yeni başlayanlar için ders seçimi ders yılının ilk haftasında yapılır.
+9. Disiplin ve Ödül:
+    -Disiplin Cezaları Kınama, kısa süreli uzaklaştırma, okul değiştirme, örgün eğitim dışına çıkarma.
+    -Kopya ve Sigara Kopya çekmek veya tütün mamulü kullanmak "Kınama" cezası gerektirir.
+    -Teşekkür Belgesi Ağırlıklı ortalaması 70,00 - 84,99 arası olanlara verilir
+    -Takdir Belgesi Ağırlıklı ortalaması 85,00 ve üzeri olanlara verilir.
+
+
+
+
+
+
 
 Bağlam:
 {baglam}
